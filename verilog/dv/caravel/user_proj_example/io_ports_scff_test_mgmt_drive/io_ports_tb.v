2f242d7df2437e59928a82c52842649ead80e512
manarabdelaty <manarabdelatty@aucegypt.edu>
2021-02-24 14:53:00 +0200

    Added scff_test with mgmt stimulus

--- verilog/dv/caravel/user_proj_example/io_ports_scff_test_mgmt_drive/io_ports_tb.v
+++ verilog/dv/caravel/user_proj_example/io_ports_scff_test_mgmt_drive/io_ports_tb.v
@@ -24,11 +24,12 @@
 
 `define FPGA_SCANCHAIN_SIZE 1024
 
-`define POWER_UP_TIME_PERIOD 200
+`define POWER_UP_TIME_PERIOD 100
 `define SOC_RESET_TIME_PERIOD 2000
 `define SOC_SETUP_TIME_PERIOD 200*2001
 `define SOC_CLOCK_PERIOD 12.5
 `define FPGA_CLOCK_PERIOD 12.5
+// 360
 
 module scff_test_post_pnr_caravel_autocheck_top_tb;
   reg clock;
@@ -38,31 +39,25 @@ module scff_test_post_pnr_caravel_autocheck_top_tb;
 
   wire gpio;
   wire [37:0] mprj_io;
+  wire [6:0] checkbits;
 
+  assign checkbits = mprj_io[10:4];
+  
   // ----- Local wires for control ports of FPGA fabric -----
-  wire [0:0] pReset;
   reg [0:0] prog_clock_reg;
   wire [0:0] prog_clk;
   wire [0:0] prog_clock;
-  wire [0:0] Test_en;
-  wire [0:0] Reset;
   reg [0:0] op_clock_reg;
   wire [0:0] op_clk;
   wire [0:0] op_clock;
   reg [0:0] prog_reset;
   reg [0:0] greset;
 
-  // ---- Configuration-chain head -----
-  wire [0:0] ccff_head;
-  // ---- Configuration-chain tail -----
-  wire [0:0] ccff_tail;
-
   // ---- Scan-chain head -----
   reg [0:0] sc_head;
   // ---- Scan-chain tail -----
   wire [0:0] sc_tail;
 
-  wire [0:0] IO_ISOL_N;
 
   // ----- Counters for error checking -----
   integer num_clock_cycles = 0; 
@@ -114,80 +109,22 @@ module scff_test_post_pnr_caravel_autocheck_top_tb;
   initial
     begin
       greset[0] = 1'b1;
-      #(`SOC_SETUP_TIME_PERIOD + 2 * `FPGA_CLOCK_PERIOD)  greset[0] = 1'b0;
+      wait(checkbits === 7'h4);
+      $display("Test started.. ");
+      #(2 * `FPGA_CLOCK_PERIOD)  greset[0] = 1'b0;
+      wait(checkbits === 7'h6);
+      #5000;
+      $display("Simulation ended successfuly..");
+      $finish;
     end
   // ----- End operating reset signal generation -----
   
   // ----- Begin connecting global ports of FPGA fabric to stimuli -----
   assign op_clk[0] = op_clock[0];
   assign prog_clk[0] = prog_clock[0];
-  assign pReset[0] = ~prog_reset[0];
-  assign Reset[0] = ~greset[0];
-  assign Test_en[0] = ~greset;
-  assign ccff_head[0] = 1'b0;
-  assign IO_ISOL_N[0] = ~greset;
   // ----- End connecting global ports of FPGA fabric to stimuli -----
-
-  assign mprj_io[0] = Test_en;
-  assign mprj_io[1] = IO_ISOL_N;
-  assign mprj_io[2] = Reset;
-  assign mprj_io[3] = pReset;
-  assign mprj_io[12] = ccff_head;
-  assign mprj_io[25] = 1'b0; // Set FPGA to interface logic analyzer by default
-  assign mprj_io[26] = sc_head;
   assign mprj_io[36] = op_clk;
   assign mprj_io[37] = prog_clk;
-
-  assign sc_tail = mprj_io[11];
-  assign ccff_tail = mprj_io[35];
-
-  assign mprj_io[10:4] = {7{1'b0}}; 
-  assign mprj_io[24:13] = {12{1'b0}}; 
-  assign mprj_io[34:27] = {8{1'b0}}; 
-
-// Generate a pulse after operating reset is disabled (in the 2nd clock
-// cycle). Then the head of scan chain should be always zero
-  always @(negedge op_clock[0]) begin
-    sc_head = 1'b1;
-    if (0 != num_clock_cycles) begin
-      sc_head = 1'b0;
-    end
-  end
-
-// ----- Count the number of programming cycles -------
-  always @(posedge op_clock[0]) begin
-    num_clock_cycles = num_clock_cycles + 1; 
-    // Indicate when scan chain loading is suppose to end
-    if (`FPGA_SCANCHAIN_SIZE + 1 == num_clock_cycles) begin
-      scan_done = 1'b1;
-    end
-
-    // Check the tail of scan-chain when configuration is done 
-    if (1'b1 == scan_done) begin
-      // The tail should spit a pulse after configuration is done
-      // So it should be at logic '1' and then pulled down to logic '0'
-      if (0 == num_checked_points) begin
-        if (sc_tail !== 1'b1) begin
-          $display("Error: sc_tail = %b", sc_tail);
-          num_errors = num_errors + 1;
-        end
-      end
-      if (1 <= num_checked_points) begin
-        if (sc_tail !== 1'b0) begin
-          $display("Error: sc_tail = %b", sc_tail);
-          num_errors = num_errors + 1;
-        end
-      end
-      num_checked_points = num_checked_points + 1;
-    end
-
-    if (2 < num_checked_points) begin
-      $display("Simulation finish with %d errors", num_errors);
-
-      // End simulation
-      $finish;
-    end
-  end
 
   // External clock is used by default.  Make this artificially fast for the
   // simulation.  Normally this would be a slow clock and the digital PLL
@@ -198,6 +135,11 @@ module scff_test_post_pnr_caravel_autocheck_top_tb;
   initial begin
     clock = 0;
   end
+
+  initial begin
+		$dumpfile("io_ports.vcd");
+		$dumpvars(0, scff_test_post_pnr_caravel_autocheck_top_tb);
+	end
 
   initial begin
     RSTB <= 1'b0;
